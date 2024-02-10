fb29b3994128bc47df2f0ec64cde92a85c94e7b8
Ismail Kassim <ikassim9@gmail.com>
2024-02-10 16:02:46 -0600

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Home from '../src/pages/Home';
+import Register from './pages/Register';
 
 export default function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path='/' element= {<Home />}></Route>
+      <Route path='/users/register' element= {<Register />}  > </Route>
     </Routes>
+    </BrowserRouter>
   );
 }
