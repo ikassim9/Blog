113a774d95627e960e0925d940d27a057ed9aaf0
Ismail Kassim <ikassim9@gmail.com>
2024-02-10 14:19:39 -0600

    style update

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,13 +1,15 @@
-import Button from "./Button";
-
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <header className="flex p-4 items-center">
       <h1 className="mr-auto ml-5 text-xl">Blog</h1>
       <nav className="flex gap-8">
-       <button className="text-xl">Login</button>
-        <Button width ="w-20" bgColor="bg-primary" textColor="text-white" value = "Sign up"/>
+       {/* <button className="text-xl">Login</button>
+        <Button width ="w-20" bgColor="bg-primary" textColor="text-white" value = "Sign up"/> */}
+        <Link to="/users/login" className="text-center p-2" >Login</Link>
+        <Link  to="/users/register" className="text-base text-center bg-primary text-white w-28 p-2 rounded-md">Sign up</Link>
+
       </nav>
     </header>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'primary': '#1B2034'
-      }
+        'primary': '#1B2034',
+         'background': '#f2f2f2'
+      },
+
+    fontFamily: {
+        robot: ['Roboto', 'sans-serif'],
+      },
 
     },
   },
