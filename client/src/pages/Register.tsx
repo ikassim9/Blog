07009c4dc6f499817d5f72a8a87abafd2204a8f1
Ismail Kassim <ikassim9@gmail.com>
2024-03-05 20:19:31 -0600

    code clean up

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,24 +2,22 @@ import { Link } from "react-router-dom";
 import Auth from "../services/Auth";
 import FirebaseAuth from "../services/FirebaseAuth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { SubmitHandler, useForm } from "react-hook-form"
- 
-export default function Register() {
+import { SubmitHandler, useForm } from "react-hook-form";
 
+export default function Register() {
   // use to support typescript
   type FormValues = {
-    name: string
-    email: string
-    password: string,
-    emailExistError: string
-  }
+    name: string;
+    email: string;
+    password: string;
+    emailExistError: string;
+  };
 
   // validate only on form submission
   const {
     register,
     handleSubmit,
     setError,
-    reset,
     clearErrors,
     formState: { errors },
   } = useForm<FormValues>({
@@ -28,19 +26,12 @@ export default function Register() {
   });
 
   const registerUser: SubmitHandler<FormValues> = async (form: FormValues) => {
-
     const url = "/user/register";
 
     const { name, email, password } = form;
- 
+
     createUserWithEmailAndPassword(FirebaseAuth, email, password)
       .then(async (userCredential) => {
-        // Signed up
-
-        // clear existing email error if present
-
-       
-
         const user = userCredential.user;
 
         const authToken = await user.getIdToken();
@@ -48,26 +39,20 @@ export default function Register() {
         await Auth.register(url, name, authToken)
           .then((response) => console.log(response.data))
           .catch((error) => {});
-
-
-          reset();
         // ...
       })
       .catch((error) => {
         switch (error.code) {
           case "auth/email-already-in-use": {
-
-            setError("emailExistError", { type: "Server", message: "Email already exists" })
-
-            // setError("email", {
-            //   message: "Email already exists",
-            // });
+            setError("emailExistError", {
+              type: "Server",
+              message: "Email already exists",
+            });
           }
         }
 
         console.error(error.code);
       });
-
   };
 
   return (
@@ -94,9 +79,10 @@ export default function Register() {
                 type="text"
                 placeholder="John"
                 data-cy="input_name"
- 
               />
-              <p data-cy='error_name' className="mt-3 text-red-500">{errors.name?.message}</p>
+              <p data-cy="error_name" className="mt-3 text-red-500">
+                {errors.name?.message}
+              </p>
             </div>
 
             <div>
@@ -108,24 +94,23 @@ export default function Register() {
                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                     message: "Please enter a valid email",
                   },
-                  
+
                   onChange: () => {
-                    if(errors.emailExistError?.message){
+                    if (errors.emailExistError?.message) {
                       clearErrors("emailExistError");
                     }
-
-                  }
-                   
+                  },
                 })}
                 className="field"
                 type="email"
                 placeholder="john1234@gmail.com"
-                data-cy='input_email'
+                data-cy="input_email"
               />
 
- 
-              <p data-cy='error_email'  className="mt-3 text-red-500">{errors.email?.message || errors.emailExistError?.message}</p>
-             </div>
+              <p data-cy="error_email" className="mt-3 text-red-500">
+                {errors.email?.message || errors.emailExistError?.message}
+              </p>
+            </div>
 
             <div>
               <label htmlFor="password">Password</label>
@@ -140,12 +125,17 @@ export default function Register() {
                 className="field"
                 type="password"
                 placeholder="6+ characters"
-                data-cy='input_password'
+                data-cy="input_password"
               />
-              <p  data-cy='error_password' className="mt-3 text-red-500">{errors.password?.message}</p>
+              <p data-cy="error_password" className="mt-3 text-red-500">
+                {errors.password?.message}
+              </p>
             </div>
 
-            <button data-cy='submitBtn' className="bg-primary text-white  p-2 rounded-md w-full">
+            <button
+              data-cy="submitBtn"
+              className="bg-primary text-white  p-2 rounded-md w-full"
+            >
               Sign up
             </button>
             <span className="block">
