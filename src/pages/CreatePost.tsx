cf74f305eb51b18d425bbacbafa8f570681e44dc
Ismail Kassim <ikassim9@gmail.com>
2025-02-02 18:30:36 -0600

    add remove thumnail option

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -13,10 +13,12 @@ export default function CreatePost() {
   const [message, setMessage] = useState('');
   const  titleRef = useRef<any>(null);
   const descriptionRef = useRef<any>(null); 
-  const [selectedImage, setSelectedImage] = useState<any>();
   const formRef = useRef<any>();
   const [isSubmitting, setIsSubmittting] = useState(false);
-  
+  const [previewURL, setPreviewURL] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null); // Store actual file
+  const fileInputRef = useRef<any>();
+
   const navigate = useNavigate();
 
   const handleFileChange = (event: any) => {
@@ -24,10 +26,22 @@ export default function CreatePost() {
  
     const file = event.target.files[0];
     if (file) {
-      setSelectedImage(file);
+      const imgURL = URL.createObjectURL(file);
+      setPreviewURL(imgURL);
+      setSelectedFile(file);
+ 
     }
   };
 
+  const removeThumbnail = () => {
+
+    if (previewURL) 
+      URL.revokeObjectURL(previewURL);
+
+    fileInputRef.current.value = "";
+    setPreviewURL(null);
+    setSelectedFile(null);
+   };
  
   const createPost= async (event: any) => {
     event.preventDefault();
@@ -38,7 +52,8 @@ export default function CreatePost() {
     const formData = new FormData();
     formData.append("title", title);
     formData.append("description", description);
-    formData.append("image", selectedImage);
+    if(selectedFile)
+    formData.append("image", selectedFile);
 
     if (title == null || title === "") {
       setMessage("Please enter a title");
@@ -63,83 +78,98 @@ export default function CreatePost() {
   };
   return (
     <>
-    <div className="min-h-screen flex flex-col">
-      <header>
-      <Nav />
-      </header>
-      <main className="flex-grow flex items-center justify-center">
-      <div className="p-4 w-full max-w-4xl ">
+      <div className="min-h-screen flex flex-col">
+        <header>
+          <Nav />
+        </header>
+        <main className="flex-grow flex items-center justify-center">
+          <div className="p-4 w-full max-w-4xl ">
+            {message && (
+              <p className=" border-t border-b border-blue-500 text-blue-700 text-center text-lg mb-6">
+                {message}
+              </p>
+            )}
 
-      {message && <p className=" border-t border-b border-blue-500 text-blue-700 text-center text-lg mb-6">{message}</p>}
+            <h1 className="text-center text-3xl mb-4">Create post</h1>
 
- 
-        <h1 className="text-center text-3xl mb-4">Create post</h1>
+            <form
+              action="POST"
+              className="space-y-4 group"
+              onSubmit={createPost}
+              noValidate
+              ref={formRef}
+            >
+              <div className="mb-4">
+                <label className="block mb-2">
+                  Upload Thumbnail (Optional)
+                </label>
 
-        <form
-          action="POST"
-          className="space-y-4 group"
-          onSubmit={createPost}
-          noValidate
-          ref={formRef}
-        >
- <div className="mb-4">
-  <label className="block mb-2">Upload Thumbnail (Optional)</label>
+                {/* Hidden file input */}
+                <input
+                  type="file"
+                  id="thumbnail-upload"
+                  accept="image/*"
+                  className="hidden"
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                />
 
-  {/* Hidden file input */}
-  <input
-    type="file"
-    id="thumbnail-upload"
-    accept="image/*"
-    className="hidden"
-    onChange={handleFileChange}
-  />
+                {/* Label acts as the upload area */}
+                <div className="flex gap-8">
+                  <label
+                    htmlFor="thumbnail-upload"
+                    className="flex justify-center items-center cursor-pointer bg-gray-100 border-2 border-gray-300 rounded-md hover:bg-gray-200 transition duration-200 w-32 h-32"
+                  >
+                    {previewURL ? (
+                      <img
+                        src={previewURL}
+                        alt="Thumbnail preview"
+                        className="w-full h-full object-cover"
+                      />
+                    ) : (
+                      <span className="text-gray-500">Choose Image</span>
+                    )}
+                  </label>
 
-  {/* Label acts as the upload area */}
-  <label
-    htmlFor="thumbnail-upload"
-    className="flex justify-center items-center cursor-pointer bg-gray-100 border-2 border-gray-300 rounded-md hover:bg-gray-200 transition duration-200 w-32 h-32"
-  >
-    {/* Render image if available */}
-    {selectedImage ? (
-      <img
-        src={URL.createObjectURL(selectedImage)}
-        alt="Thumbnail preview"
-        className="w-full h-full"
-      />
-    ) : (
-      // Placeholder if no image is uploaded
-      <span className="text-gray-500">Choose Image</span>
-    )}
-  </label>
-</div>
-          <input
-            ref={titleRef}
-            type="text"
-            maxLength={150}
-            placeholder="Title"
-            required
-            id="title"
-            className="p-2 rounded  w-full mt-1 bg-transparent border "
-            name="title"
-          />
+                  {previewURL && (
+                    <button
+                      type="button"
+                      onClick={removeThumbnail}
+                      className="p-1 mt-auto w-24 text-white text-center bg-red-500 rounded-md hover:bg-red-300"
+                    >
+                      Remove
+                    </button>
+                  )}
+                </div>
+              </div>
+              <input
+                ref={titleRef}
+                type="text"
+                maxLength={150}
+                placeholder="Title"
+                required
+                id="title"
+                className="p-2 rounded  w-full mt-1 bg-transparent border "
+                name="title"
+              />
 
-          <TextEditor
-            editable={true}
-            content=""
-            ref={descriptionRef}
-          ></TextEditor>
-          <button
-            type="submit"
-            data-cy="submitBtn"
-            disabled={isSubmitting}
-            className="bg-primary text-white  p-2 rounded-md w-full"
-          >
-            {isSubmitting ? "Publishing..." : "Publish"}
-          </button>
-        </form>
-      </div>
-      </main>
-      <Footer />
+              <TextEditor
+                editable={true}
+                content=""
+                ref={descriptionRef}
+              ></TextEditor>
+              <button
+                type="submit"
+                data-cy="submitBtn"
+                disabled={isSubmitting}
+                className="bg-primary text-white  p-2 rounded-md w-full"
+              >
+                {isSubmitting ? "Publishing..." : "Publish"}
+              </button>
+            </form>
+          </div>
+        </main>
+        <Footer />
       </div>
     </>
   );
