86b524893ccb16aee545e0aface66598d1bff878
Ismail Kassim <ikassim9@gmail.com>
2025-11-23 20:58:44 -0600

    to trigger re deploy

--- src/services/FirebaseAuth.tsx
+++ src/services/FirebaseAuth.tsx
@@ -21,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
 const FirebaseAuth = getAuth(app);
 
